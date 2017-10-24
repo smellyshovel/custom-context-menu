54b1fc603d432c78acbc5cd8a239e6667e2079a6
Matthew Mamonov <g.smellyshovel@gmail.com>
2017-10-24 21:39:22 +0300

    The first tile is ready

--- scripts/master.js
+++ scripts/master.js
@@ -2,6 +2,7 @@ var tile1 = document.querySelector("#tile-1");
 
 new ContextMenu(tile1, {
     id: "tile-1",
+    transfer: true,
     overlay: true,
 
     items: [
@@ -18,5 +19,28 @@ new ContextMenu(tile1, {
                 alert("OK");
             }
         },
+
+        {
+            title: "Docs",
+            function() {
+                console.log("good");
+            }
+        },
+
+        "divider",
+
+        {
+            title: "Show on GitHub",
+            function() {
+                window.location = "https://github.com/smellyshovel/custom-context-menu";
+            }
+        },
+
+        {
+            title: "Version",
+            function() {
+                alert("This Demo site uses the latest available stable v" + "VERSION"); // TODO: use API to get the latest version number
+            }
+        }
     ]
 });
